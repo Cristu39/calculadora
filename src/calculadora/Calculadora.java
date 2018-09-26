b4cf7588bb127e37d45281a5942a6edcf565d77a
Cristian <cdcscooter@gmail.com>
2018-09-26 17:06:34 +0200

    Creació de control dels nombres

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,12 +1,31 @@
 package calculadora;
+import static java.lang.Character.isDigit;
 import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
+        boolean yes = false, zero = false;
         Scanner in = new Scanner(System.in);
         System.out.println("Entra un primer nombre");
-        int n1 = in.nextInt();
+        int n1= 0;
+        do{
+            try{
+                n1 = in.nextInt();
+                yes = true;
+            }catch(Exception e){
+                System.out.println("Prova a introduir un numero enter");
+            }
+        }while(!yes);
         System.out.println("Entra un segon nombre");
         int n2 = in.nextInt();
+        do{
+            if (n2 == 0) {
+                System.out.println("Introdueix un nombre diferent a 0");
+                n2 = in.nextInt();
+            }else{
+                zero = true;
+            }
+        }while(!zero);
+        
         System.out.println("La suma dels dos valors és de: " + suma(n1,n2));
         System.out.println("La resta dels dos valors és de: " + resta(n1,n2));
         System.out.println("La multiplicació dels dos valors és de: " + multiplicacio(n1, n2));
@@ -21,7 +40,7 @@ public class Calculadora {
     public static float multiplicacio(int a, int b){
         return a * b;
     }
-    public static float divisio(int a, int b){
+    public static double divisio(int a, int b){
         return a/b;
     }
 }
