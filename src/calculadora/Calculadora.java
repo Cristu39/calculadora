4fb4770e78866d6785ab97774eaaaf79602d087b
Cristian <cdcscooter@gmail.com>
2018-09-26 16:41:32 +0200

    Creació del mètode multiplicació, crida des del main d'aquest, eliminació de les strings i refactorització del nom dels nombres que entra l'usuari

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String cad1, cad2;
         System.out.println("Entra un primer nombre");
-        int a = in.nextInt();
+        int n1 = in.nextInt();
         System.out.println("Entra un segon nombre");
-        int b = in.nextInt();
-        System.out.println("La suma dels dos valors és de: " + suma(a,b));
-        System.out.println("La resta dels dols valors és de: " + resta(a,b));
+        int n2 = in.nextInt();
+        System.out.println("La suma dels dos valors és de: " + suma(n1,n2));
+        System.out.println("La resta dels dos valors és de: " + resta(n1,n2));
+        System.out.println("La multiplicació dels dos valors és de: " + multiplicacio(n1, n2));
     }
     private static int suma(int a, int b){
         return a+b;
     }
     private static int resta(int a, int b){
         return a - b;
+    }
+    public static float multiplicacio(int a, int b){
+        return a * b;
     }
 }
