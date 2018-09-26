80cc8d41db0f09e4cf1b9cb3f79043a08e632f5d
Cristian <cdcscooter@gmail.com>
2018-09-26 16:28:14 +0200

    Creació del mètode suma i crida desde el main

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Entra un primer numero");
+        System.out.println("Entra un primer nombre");
         int a = in.nextInt();
-        System.out.println("Entra un segundo numero");
+        System.out.println("Entra un segon nombre");
         int b = in.nextInt();
         System.out.println("El primer numero més el segon dónan com a resultat: " + suma(a,b));
     }
-    public static int suma(int a, int b){
+    private static int suma(int a, int b){
         return a+b;
     }
 }
