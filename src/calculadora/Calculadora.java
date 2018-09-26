c16ec806744bb1a75785971379eb6647d0cca6bc
Cristian <cdcscooter@gmail.com>
2018-09-26 16:19:22 +0200

    Creació variables a i b recollides per teclat

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,6 +1,12 @@
 package calculadora;
+import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
-       
+        Scanner in = new Scanner(System.in);
+        System.out.println("Entra un primer numero");
+        int a = in.nextInt();
+        System.out.println("Entra un segundo numero");
+        int b = in.nextInt();
+        
     }
 }
