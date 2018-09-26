2cbddbe544c4cc1d433c307c6caaccba9076eab4
Cristian <cdcscooter@gmail.com>
2018-09-26 16:30:18 +0200

    Creació de les variables irrellevants

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String cad1, cad2;
         System.out.println("Entra un primer nombre");
         int a = in.nextInt();
         System.out.println("Entra un segon nombre");
